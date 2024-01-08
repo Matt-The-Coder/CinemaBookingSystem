07e4169ef2ac3314ffae7d566ca6d64083106081
Ralph Matthew Manabat <120724337+Matt-The-Coder@users.noreply.github.com>
2024-01-08 11:16:16 +0800

    Update connection.js

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql");
 
+// const db = mysql.createPool({
+//     user: "admin",
+//     password: "UXl8TdEc",
+//     host: "mysql-135607-0.cloudclusters.net",
+//     port: "17449",
+//     database: "cinemabook"
+// })
+
 const db = mysql.createPool({
-    user: "admin",
-    password: "UXl8TdEc",
-    host: "mysql-135607-0.cloudclusters.net",
-    port: "17449",
-    database: "cinemabook"
-})
+        host: 'bjxwp9zadzdyfnlsid8b-mysql.services.clever-cloud.com',
+        user:'uyo3diweamt0qrca',
+        password:'SSSLkR8oQLwUFVl0kyWU',
+        database: "bjxwp9zadzdyfnlsid8b",
+        port: "3306"
+    })
 
 module.exports = (sql)=>{
     return new Promise((resolve, reject)=>{
